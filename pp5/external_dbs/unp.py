a3e8b97f565de1c35ce7ab235947f884933831cc
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2020-02-14 19:11:57 +0200

    unp: find ena xrefs

--- pp5/external_dbs/unp.py
+++ pp5/external_dbs/unp.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from typing import Set, List, Iterable
 from urllib.parse import urlsplit
 
 import requests
-from Bio import SwissProt as SwissProt
+import Bio.SwissProt
+from Bio.SwissProt import Record as UNPRecord
 from requests import HTTPError
 
 from pp5 import UNP_DIR
@@ -45,7 +47,7 @@ def unp_download(unp_id: str, unp_dir=UNP_DIR) -> Path:
             return unp_download(new_unp_id, unp_dir)
 
 
-def unp_record(unp_id: str, unp_dir=UNP_DIR) -> SwissProt.Record:
+def unp_record(unp_id: str, unp_dir=UNP_DIR) -> UNPRecord:
     """
     Create a Record object holding the information about a protein based on
     its Uniprot id.
@@ -56,4 +58,28 @@ def unp_record(unp_id: str, unp_dir=UNP_DIR) -> SwissProt.Record:
     filename = unp_download(unp_id, unp_dir)
 
     with open(filename, 'r') as local_handle:
-        return SwissProt.read(local_handle)
+        return Bio.SwissProt.read(local_handle)
+
+
+def find_ena_xrefs(unp_rec: UNPRecord, molecule_types: Iterable[str]) \
+        -> List[str]:
+    """
+    Find EMBL ENA cross-references to specific molecule types in a Uniprot
+    record.
+    :param unp_rec: The Uniprot record.
+    :param molecule_types: Which types of molecules are allowed for the
+    returned references. For example, 'mrna' or 'genomic_dna'.
+    :return: A list of ENA ids which can be used to retrieve ENA records.
+    """
+
+    ena_ids = []
+    cross_refs = unp_rec.cross_references
+    molecule_types = {t.lower() for t in molecule_types}
+
+    embl_refs = (x for x in cross_refs if x[0].lower() == 'embl')
+    for dbname, id1, id2, comment, molecule_type in embl_refs:
+        molecule_type = molecule_type.lower()
+        if molecule_type in molecule_types and id2 and len(id2) > 3:
+            ena_ids.append(id2)
+
+    return ena_ids
