ca1979b8925867afa664c63b9a027e304a6b47a9
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2020-02-22 14:48:23 +0200

    datasets: Improve multi-process collection
    Workers now use a shared data folder but a worker-specific
    download folder to prevent write collisions.
    
    After completion of all workers we copy the downloaded resources
    to the data folder for next time.

--- pp5/datasets.py
+++ pp5/datasets.py
@@ -1,25 +1,61 @@
 import logging
 import multiprocessing as mp
+import multiprocessing.pool
+import os
+import shutil
+import tempfile
 import time
+from pathlib import Path
 
+import pp5
 from pp5.external_dbs import pdb
+from pp5.external_dbs.pdb import PDBQuery
 from pp5.protein import ProteinRecord, ProteinInitError
 
 LOGGER = logging.getLogger(__name__)
 
 
-def collect_data():
-    # Query PDB for structures
-    query = pdb.PDBCompositeQuery(
-        pdb.PDBExpressionSystemQuery('Escherichia Coli'),
-        pdb.PDBResolutionQuery(max_res=1.0)
-    )
+def worker_process_init(base_workers_dl_dir, *args):
+    # Provide each process with a unique download folder.
+    pid = os.getpid()
+    worker_dl_dir = base_workers_dl_dir.joinpath(f'{pid}')
+    os.makedirs(worker_dl_dir, exist_ok=True)
+    LOGGER.info(f'Worker process {pid} using dir {worker_dl_dir}...')
+    # Override the default with the process-specific dir to have a
+    # different download directory for each process.
+    pp5.BASE_DOWNLOAD_DIR = worker_dl_dir
 
+
+def clean_worker_downloads(base_workers_dl_dir):
+    n_copied = 0
+    downloaded_files = base_workers_dl_dir.glob('**/*/*.*')
+    for downloaded_file in downloaded_files:
+        rel_path = downloaded_file.relative_to(base_workers_dl_dir)
+        # Remove pid dir and append the relative path to the main data dir
+        data_path = pp5.BASE_DATA_DIR.joinpath(*rel_path.parts[1:])
+        if data_path.is_file():
+            continue
+        # The downloaded file is not in the data dir, so copy it.
+        shutil.copy2(str(downloaded_file), str(data_path))
+        n_copied += 1
+    LOGGER.info(f'Copied {n_copied} downloaded files from '
+                f'{base_workers_dl_dir} into {pp5.BASE_DATA_DIR}')
+    shutil.rmtree(base_workers_dl_dir)
+    LOGGER.info(f'Deleted temp folder {base_workers_dl_dir}')
+
+
+def collect_data(query: PDBQuery):
     pdb_ids = query.execute()
+
     LOGGER.info(f"Got {len(pdb_ids)} structures from PDB")
 
+    base_workers_dl_dir = Path(tempfile.gettempdir()).joinpath('pp5_data')
+    os.makedirs(str(base_workers_dl_dir), exist_ok=True)
+
     async_results = []
-    with mp.pool.Pool(processes=8) as pool:
+    with mp.pool.Pool(processes=8,
+                      initializer=worker_process_init,
+                      initargs=(base_workers_dl_dir,)) as pool:
         for i, pdb_id in enumerate(pdb_ids):
             async_results.append(
                 pool.apply_async(ProteinRecord.from_pdb, (pdb_id,))
@@ -40,10 +76,19 @@ def collect_data():
             pps = counter / (time.time() - start_time)
             LOGGER.info(f'Collected {protein_recs} ({pps:.1f} proteins/sec)')
 
-            # TODO: Write to file
+            # Write to file
+            for prec in protein_recs:
+                prec.to_csv(pp5.data_subdir('proteins'))
 
+        clean_worker_downloads(base_workers_dl_dir)
         LOGGER.info(f"Done: {counter} proteins collected.")
 
 
 if __name__ == '__main__':
-    collect_data()
+    # Query PDB for structures
+    query = pdb.PDBCompositeQuery(
+        pdb.PDBExpressionSystemQuery('Escherichia Coli'),
+        pdb.PDBResolutionQuery(max_res=1.0)
+    )
+
+    collect_data(query)
