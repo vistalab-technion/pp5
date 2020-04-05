f0933e21824d3c55ae624b27938f86bcce81d838
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2020-04-05 08:49:05 +0300

    pdb tests: parametrize some tests

--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -220,33 +220,33 @@ class TestPDBToUNP:
     def test_no_chain_multi_unp(self):
         test_id = '4HHB'
         expected_unp_ids = {'P69905', 'P68871'}
+        actual_unp_id = pdb.pdbid_to_unpid(test_id)
+        assert actual_unp_id in expected_unp_ids
 
-        id = pdb.pdbid_to_unpid(test_id)
-        assert id in expected_unp_ids
+    @pytest.mark.parametrize('test_id', ['4HHB:A', '4HHB:C'])
+    def test_with_chain_multi_unp_1(self, test_id):
+        self._check(test_id, 'P69905')
 
-    def test_with_chain_multi_unp(self):
-        for test_id in {'4HHB:A', '4HHB:C'}:
-            self._check(test_id, 'P69905')
-
-        for test_id in {'4HHB:B', '4HHB:D'}:
-            self._check(test_id, 'P68871')
+    @pytest.mark.parametrize('test_id', ['4HHB:B', '4HHB:D'])
+    def test_with_chain_multi_unp_2(self, test_id):
+        self._check(test_id, 'P68871')
 
     def test_with_invalid_chain(self):
         with pytest.raises(ValueError, match='chain Z'):
             pdb.pdbid_to_unpid('4HHB:Z')
 
-    def test_with_no_xref_in_file(self):
-        for test_id in {'5LTR', '5LTR:A'}:
-            self._check(test_id, 'B1PNC0')
+    @pytest.mark.parametrize('test_id', ['5LTR', '5LTR:A'])
+    def test_with_no_xref_in_file(self, test_id):
+        self._check(test_id, 'B1PNC0')
 
-    def test_with_no_xref_in_file_and_pdb(self):
-        for pdb_id in {'5EJU', '4DXP'}:
-            with pytest.raises(ValueError):
-                pdb.pdbid_to_unpid(pdb_id)
+    @pytest.mark.parametrize('test_id', ['5EJU', '4DXP'])
+    def test_with_no_xref_in_file_and_pdb(self, test_id):
+        with pytest.raises(ValueError):
+            pdb.pdbid_to_unpid(test_id)
 
-    def test_with_no_struct_ref_entry(self):
-        for test_id in {'3G53', '3G53:A'}:
-            self._check(test_id, 'P02213')
+    @pytest.mark.parametrize('test_id', ['3G53', '3G53:A'])
+    def test_with_no_struct_ref_entry(self, test_id):
+        self._check(test_id, 'P02213')
 
     def test_multi_unp_for_single_chain(self):
         self._check('3SG4:A', 'P0DP29')
