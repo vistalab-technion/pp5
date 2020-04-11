1a3910e65e737222969e2fc80454a2db3c497963
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2020-04-11 23:58:25 +0300

    cli: minor logging update

--- pp5.py
+++ pp5.py
@@ -237,4 +237,4 @@ if __name__ == '__main__':
         # Invoke it with the remaining arguments
         handler_fn(**parsed_args)
     except Exception as e:
-        _LOG.error(f'{e}')
+        _LOG.error(f'{e.__class__.__name__}: {e}')
