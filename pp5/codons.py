731294e18789295cfb53347ed3ede4bac8e3d6a9
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2020-06-30 15:44:22 +0300

    codons.py: various codon-related utilities

---
+++ pp5/codons.py
@@ -0,0 +1,29 @@
+import re
+
+from Bio.Data import CodonTable
+from Bio.Data import IUPACData
+
+
+def codon2aac(codon: str):
+    """
+    Converts codon to AA-CODON, which we will use as codon identifiers.
+    :param codon: a codon string.
+    :return: a string formatted AA-CODON where AA is the
+    corresponding amino acid.
+    """
+    aa = CODON_TABLE[codon]
+    return f'{aa}-{codon}'.upper()
+
+
+CODON_TABLE = CodonTable.standard_dna_table.forward_table
+START_CODONS = CodonTable.standard_dna_table.start_codons
+STOP_CODONS = CodonTable.standard_dna_table.stop_codons
+ACIDS_1TO3 = IUPACData.protein_letters_1to3
+CODONS = sorted(CODON_TABLE.keys())
+N_CODONS = len(CODONS)
+AA_CODONS = sorted(codon2aac(c) for c in CODONS)
+ACIDS = sorted(set([aac[0] for aac in AA_CODONS]))
+
+CODON_RE = re.compile(rf'(?:(?P<aa>[{str.join("", ACIDS)}])-)?'
+                      rf'(?P<codon>{str.join("|", CODONS)})',
+                      re.VERBOSE | re.IGNORECASE)
