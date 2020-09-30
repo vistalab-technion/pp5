430f6509985a2d20f0eff669fe116d5dff7bc75a
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2020-09-30 10:50:14 +0300

    tests: Improved KDE tests
    Estimate with real data drawn from a BvM mixture distribution.

--- tests/test_vonmises.py
+++ tests/test_vonmises.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 from pytest import approx, raises
 
-from pp5.vonmises import BvMMixtureDiscreteDistribution
+from pp5.vonmises import BvMKernelDensityEstimator, BvMMixtureDiscreteDistribution
 
 
 class TestBvMMixtureDiscreteDistribution:
@@ -88,5 +88,50 @@ class TestBvMMixtureDiscreteDistribution:
         assert max_sample <= (self.dist.grid[-1] + 2 * np.pi / self.dist.gridsize)
 
     @pytest.mark.parametrize("n", [0, 10])
-    def test_plot(self, setup, n):
-        fig, ax = self.dist.plot(sample_size=n)
+    def test_plot_number(self, setup, n):
+        fig, ax = self.dist.plot(samples=n)
+
+
+class TestBvMKernelDensityEstimator(object):
+    @pytest.mark.parametrize(
+        ["n_bins", "k1", "k2", "k3", "batchsize"],
+        [
+            (0, 1, 2, 3, 16),
+            (1, 1, 2, 3, 16),
+            (100, None, 2, 3, 16),
+            (100, 1, None, 3, 16),
+            (100, 1, 2, None, 16),
+            (100, 1, 2, 3, 0),
+        ],
+    )
+    def test_init_fail(self, n_bins, k1, k2, k3, batchsize):
+        with pytest.raises(AssertionError):
+            BvMKernelDensityEstimator(n_bins, k1, k2, k3, batchsize)
+
+    def test_estimate(self):
+        kde = BvMKernelDensityEstimator(n_bins=256, k1=10, k2=20, k3=-2,)
+        phi_psi = np.random.uniform(-np.pi, np.pi, size=(500, 2))
+        p = kde(phi_psi[:, 0], phi_psi[:, 1])
+
+        assert p.shape == (kde.n_bins, kde.n_bins)
+        assert p.dtype == kde.dtype
+
+    @pytest.mark.parametrize("n_samples", [100, 1000, 5000])
+    @pytest.mark.parametrize("grid_size", [128, 256])
+    def test_estimate_real_mixture(self, grid_size, n_samples):
+        bvm = BvMMixtureDiscreteDistribution(
+            k1=[0.7, 1.0],
+            k2=[1.3, 3.3],
+            A=[2, -1],
+            mu=[[1, 2], [-0.1, -0.2]],
+            alpha=[0.3, 0.7],
+            two_pi=False,
+            gridsize=grid_size,
+        )
+        samples = bvm.sample(n_samples)
+        kde = BvMKernelDensityEstimator(n_bins=grid_size, k1=30, k2=30, k3=2,)
+        kde_pdf = kde(samples[:, 0], samples[:, 1])
+
+        dist_norm = np.linalg.norm(bvm.pdf - kde_pdf)
+
+        assert dist_norm < 1e-2
