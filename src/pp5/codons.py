258e91a56614d0632094fb9ff4aacd43f8a1b05d
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2021-05-24 12:00:16 +0300

    codons: Generation of synonymous k-tuples

--- src/pp5/codons.py
+++ src/pp5/codons.py
@@ -1,15 +1,15 @@
 import re
 import itertools as it
-from typing import Tuple, Sequence
+from typing import Set, Tuple, Iterator, Sequence, cast
 
 from Bio.Data import IUPACData, CodonTable
 
-# Separates between AA and matching codon
+# Separates between AA and matching codon, e.g. "A-GCA"
 AAC_SEP = "-"
 
 
-# Separates between two (or more) AAs or two (or more) codons
-AA_CC_SEP = "_"
+# Separates between two (or more) AAs or two (or more) codons, e.g. "A-GCA_C-TGC"
+AAC_TUPLE_SEP = "_"
 
 
 def codon2aac(codon: str):
@@ -24,12 +24,12 @@ def codon2aac(codon: str):
 
 
 CODON_TABLE = CodonTable.standard_dna_table.forward_table
-START_CODONS = CodonTable.standard_dna_table.start_codons
-STOP_CODONS = CodonTable.standard_dna_table.stop_codons
-CODONS = sorted(CODON_TABLE.keys())
+START_CODONS: Sequence[str] = CodonTable.standard_dna_table.start_codons
+STOP_CODONS: Sequence[str] = CodonTable.standard_dna_table.stop_codons
+CODONS: Sequence[str] = sorted(CODON_TABLE.keys())
 UNKNOWN_CODON = "---"
 N_CODONS = len(CODONS)
-AA_CODONS = sorted(codon2aac(c) for c in CODONS)
+AA_CODONS: Sequence[str] = sorted(codon2aac(c) for c in CODONS)
 
 ACIDS = sorted(set([aac[0] for aac in AA_CODONS]))
 N_ACIDS = len(ACIDS)
@@ -52,3 +52,74 @@ SYN_CODON_IDX: Sequence[Tuple[int, int]] = tuple(
 SYN_CODON_IDX_UNIQ: Sequence[Tuple[int, int]] = tuple(
     set(tuple(sorted(ij)) for ij in SYN_CODON_IDX)
 )
+
+
+AAC = str
+AACTuple = Tuple[AAC, ...]
+AACIndexedTuple = Tuple[int, AACTuple]
+
+
+def aac2aa(aac: AAC):
+    return aac.split(AAC_SEP)[0]
+
+
+def aac2c(aac: AAC):
+    return aac.split(AAC_SEP)[1]
+
+
+def is_synonymous(aac1: AAC, aac2: AAC):
+    return aac2aa(aac1) == aac2aa(aac2)
+
+
+def is_synonymous_tuple(aact1: AACTuple, aact2: AACTuple):
+    return all(is_synonymous(aac1, aac2) for aac1, aac2 in zip(aact1, aact2))
+
+
+def aac_tuples(k: int = 1) -> Iterator[AACTuple]:
+    """
+    Generates k-tuples of aa-codons.
+    :param k: Number of elements in each tuple.
+    :return: A generator producing the tuples.
+    """
+    return it.product(*([AA_CODONS] * k))
+
+
+def aac_tuple_pairs(
+    k: int = 1, synonymous: bool = False, unique: bool = False
+) -> Set[Tuple[AACIndexedTuple, AACIndexedTuple]]:
+    """
+
+    Creates pairs of AAC indexed k-tuples.
+    The pairs can optionally contain only synonymous codons, and can optionally be
+    unique.
+
+    :param k: Length of each tuple in each returned pair.
+    :param synonymous: Whether to only returns synonymous pairs of AAC tuples. Two
+        AAC tuples A and B are synonymous if is_synonymous_tuple(A, B) is True.
+    :param unique: Whether to only return unique pairs. If False, then a pair of
+        tuples (A, B) and another pair (B, A) will be returned, otherwise only one of
+        them.
+    :return: A set of pairs ((i, A), (j, B)) where A and B are each AAC
+        tuples of length k and i and j are their indices is the sorted order of AAC
+        tuples.
+    """
+
+    if synonymous:
+        _synonymous_fn = is_synonymous_tuple
+    else:
+        _synonymous_fn = lambda *args: True
+
+    if unique:
+        _reduce_fn = lambda x: tuple(sorted(x))
+    else:
+        _reduce_fn = lambda x: x
+
+    indexed_pairs = it.product(enumerate(aac_tuples(k)), enumerate(aac_tuples(k)))
+
+    indexed_filtered_pairs = (
+        _reduce_fn(ij)  # ij = tuple of two AACIndexedTuple
+        for ij in indexed_pairs
+        if _synonymous_fn(ij[0][1], ij[1][1])
+    )
+
+    return {*indexed_filtered_pairs}
