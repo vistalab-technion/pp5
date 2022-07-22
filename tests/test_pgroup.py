e000975c506bda6abf793f02c91348d0387bf6d5
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-07-22 12:19:01 +0300

    pgroup: small refactor in test

--- tests/test_pgroup.py
+++ tests/test_pgroup.py
@@ -8,10 +8,11 @@ class TestFromPDBRef(object):
     def setup(self):
         pass
 
-    def test_default(self):
+    @pytest.mark.parametrize("match_len", [1, 2])
+    def test_default(self, match_len):
         # Just an initial rudimentary test to check the code is alive
         pgroup = ProteinGroup.from_pdb_ref(
-            "1NKD:A", parallel=False, sa_min_aligned_residues=40
+            "1NKD:A", parallel=False, sa_min_aligned_residues=40, match_len=match_len,
         )
         assert isinstance(pgroup, ProteinGroup)
         assert pgroup.num_query_structs >= 4
