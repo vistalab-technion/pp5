581ff460a5416b7cd751e010f49dc43b691ecb27
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-09-09 19:08:56 +0300

    ProteinGroup: Add test with specific example

--- tests/test_pgroup.py
+++ tests/test_pgroup.py
@@ -20,3 +20,18 @@ class TestFromPDBRef(object):
         assert isinstance(pgroup, ProteinGroup)
         assert pgroup.num_query_structs >= 4
         assert len(pgroup.to_csv())
+
+    def test_specific_example(self):
+        pgroup = ProteinGroup.from_pdb_ref(
+            "4NE4:A",
+            parallel=False,
+            match_len=2,
+            compare_contacts=True,
+            resolution_cutoff=1.8,
+            b_max=50,
+            sa_outlier_cutoff=2.5,
+            angle_aggregation="max_res",
+            query_predicate=(lambda q: "5TEU" in q),
+        )
+
+        assert pgroup.num_matches >= 276
