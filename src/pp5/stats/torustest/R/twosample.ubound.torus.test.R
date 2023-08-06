e762597fb2d8722e6b8be66613704b9eb869b128
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2023-08-06 15:47:20 +0300

    twosample.ubound.torus.test: Update to support optionally returning the w2 distance

--- src/pp5/stats/torustest/R/twosample.ubound.torus.test.R
+++ src/pp5/stats/torustest/R/twosample.ubound.torus.test.R
@@ -9,30 +9,39 @@
 #' @return The p-value for the two-sample test on the torus.
 #'
 #' @examples
-#' 
+#'
 #' n <- 2000 # Sample size
-#' 
+#'
 #' set.seed(10)
 #'  # Bivariate von Mises distribution
 #' samp_1 <- BAMBI::rvmcos(n, kappa1 = 1, kappa2 = 1, mu1 = 0, mu2 = 0)/(2*pi)
 #' samp_2 <- BAMBI::rvmcos(n, kappa1 = 1, kappa2 = 1, mu1 = 0, mu2 = 0)/(2*pi)
-#' twosample.ubound.torus.test(samp_1, samp_2) 
+#' twosample.ubound.torus.test(samp_1, samp_2)
 #'
 #' samp_1 <- BAMBI::rvmcos(n ,kappa1 = 0, kappa2 = 0, mu1 = 0.5, mu2 = 0.5)/(2*pi)
 #' samp_2 <- BAMBI::rvmcos(n, kappa1 = 1, kappa2 = 1, mu1 = 0.5, mu2 = 0.5)/(2*pi)
-#' twosample.ubound.torus.test(samp_1, samp_2) 
-#' 
+#' twosample.ubound.torus.test(samp_1, samp_2)
+#'
 #' @export
 
-twosample.ubound.torus.test <- function(sample_1, sample_2){
-  
+twosample.ubound.torus.test <- function(sample_1, sample_2, return_dist=FALSE){
+
   n <- nrow(sample_1); m <- nrow(sample_2)
   costmatrix <- proxy::dist(x = sample_1, y = sample_2, method = dist.torus, diag = TRUE)  # Cost matrix
   wdis <- transport::wasserstein(rep(1/n, n), rep(1/m, m), costm = costmatrix, p = 2, method = 'networkflow') # Wasserstein distance
-  
+
   u_bound <- function(t, n, m){
     exp(-8 * t^2 * m * n / (n+m))
   }
-  return(u_bound(wdis^2, n, m)) #p-value
-  
+
+  pval <- u_bound(wdis^2, n, m) #p-value
+
+  # If return_dist is true, return both pval and distance, otherwise only return pvalue
+  if(return_dist){
+      return(list(pval=pval, dist=wdis))
+  }
+  else{
+      return(pval)
+  }
+
 }
