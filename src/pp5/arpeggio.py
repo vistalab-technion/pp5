0c207c85b8fe1baae0f7caa5883613fd07a81549
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2023-12-19 11:35:55 +0200

    ProteinRecord: move arpeggio df per-residue aggregation into Arpeggio class

--- src/pp5/arpeggio.py
+++ src/pp5/arpeggio.py
@@ -8,11 +8,14 @@ import subprocess
 from time import time
 from typing import Union, Optional
 from pathlib import Path
+from functools import partial
+from itertools import chain
 
 import numpy as np
 import pandas as pd
 
 import pp5
+from pp5.codons import ACIDS_3TO1, UNKNOWN_AA
 from pp5.external_dbs import pdb
 from pp5.external_dbs.pdb import PDB_RCSB
 
@@ -67,7 +70,7 @@ class Arpeggio(object):
                 f"{self.arpeggio_command}"
             )
 
-    def contact_df(self, pdb_id: str, single_sided: bool = False) -> pd.DataFrame:
+    def contacts_df(self, pdb_id: str, single_sided: bool = False) -> pd.DataFrame:
         """
         :param pdb_id: The PDB ID to run arpeggio against. Must include chain.
         :param single_sided: Whether to include only on side of each contact as in
@@ -78,6 +81,12 @@ class Arpeggio(object):
         pdb_base_id, pdb_chain_id = pdb.split_id(pdb_id)
         if not pdb_chain_id:
             raise ValueError("Must specify a chain")
+
+        LOGGER.info(
+            f"Generating contact features for {pdb_id} "
+            f"(pdb_source={self.pdb_source}, "
+            f"interaction_cutoff={self.interaction_cutoff})..."
+        )
 
         arpeggio_out_path = self._run_arpeggio(pdb_id)
 
@@ -123,6 +132,174 @@ class Arpeggio(object):
         df.set_index(index_cols, inplace=True)
 
         return df
+
+    def residue_contacts_df(self, pdb_id: str) -> pd.DataFrame:
+        """
+        Generates tertiary contact features per residue. Processes the raw arpeggio
+        output by aggregating it at the residue level.
+
+        :param pdb_id: The PDB ID to run arpeggio against. Must include chain.
+        :return: A dataframe indexed by residue id and with columns corresponding to a
+        summary of contacts per reisdue.
+        """
+        pdb_base_id, pdb_chain_id = pdb.split_id(pdb_id)
+
+        # Invoke arpeggio to get the raw contact features.
+        df_arp = self.contacts_df(pdb_id, single_sided=False)
+
+        # Create a temp df to work with
+        df = df_arp.copy().reset_index()
+
+        # Convert 'contact' column to text
+        df["contact"] = df["contact"].apply(lambda x: str.join(",", sorted(x)))
+
+        # Ignore any water contacts
+        idx_non_water = ~df["interacting_entities"].isin(
+            ["SELECTION_WATER", "NON_SELECTION_WATER", "WATER_WATER"]
+        )
+        LOGGER.info(
+            f"non-water proportion: "  #
+            f"{sum(idx_non_water) / len(idx_non_water):.2f}"
+        )
+
+        # Ignore contacts which are of type 'proximal' only
+        idx_non_proximal_only = df["contact"] != "proximal"
+        LOGGER.info(
+            f"non-proximal proportion: "
+            f"{sum(idx_non_proximal_only) / len(idx_non_proximal_only):.2f}"
+        )
+
+        # Ignore contacts starting from another chain
+        idx_non_other_chain = df["bgn.auth_asym_id"].str.lower() == pdb_chain_id.lower()
+        LOGGER.info(
+            f"start-in-chain proportion: "
+            f"{sum(idx_non_other_chain) / len(idx_non_other_chain):.2f}"
+        )
+
+        # Find contacts ending on other chain
+        idx_end_other_chain = df["end.auth_asym_id"].str.lower() != pdb_chain_id.lower()
+        LOGGER.info(
+            f"end-other-chain proportion: "
+            f"{sum(idx_end_other_chain) / len(idx_end_other_chain):.2f}"
+        )
+        contact_any_ooc = df["end.auth_asym_id"].copy()
+        contact_any_ooc[~idx_end_other_chain] = ""
+
+        # Calculate sequence distance for each contact
+        contact_sequence_dist = (df["end.auth_seq_id"] - df["bgn.auth_seq_id"]).abs()
+
+        # If end is not on chain, set sdist to NaN to clarify that it's invalid
+        contact_sequence_dist[idx_end_other_chain] = float("nan")
+
+        # Find interactions with non-AAs (ligands)
+        contact_non_aa = df["end.label_comp_id"].copy()
+        idx_end_non_aa = ~contact_non_aa.isin(list(ACIDS_3TO1.keys()))
+        contact_non_aa[~idx_end_non_aa] = ""
+        LOGGER.info(
+            f"end-non-aa proportion: "
+            f"{sum(idx_end_non_aa) / len(idx_end_non_aa):.2f}"
+        )
+
+        # Filter only contacting and assign extra features
+        df_filt = df[idx_non_water & idx_non_proximal_only & idx_non_other_chain]
+        df_filt = df_filt.assign(
+            # Note: this assign works because after filtering, the index remains intact
+            contact_sdist=contact_sequence_dist,
+            contact_any_ooc=contact_any_ooc,
+            contact_non_aa=contact_non_aa,
+        )
+        df_filt = df_filt.drop("bgn.auth_asym_id", axis="columns")
+        df_filt = df_filt.astype({"bgn.auth_seq_id": str})
+        df_filt = df_filt.set_index(["bgn.auth_seq_id"])
+        df_filt = df_filt.sort_values(by=["bgn.auth_seq_id"])
+        df_filt = df_filt.rename_axis("res_id")
+
+        # Aggregate contacts per AA
+        def _agg_join(items):
+            return str.join(",", [str(it) for it in items])
+
+        def _agg_join_aas(items):
+            return _agg_join(ACIDS_3TO1.get(aa, UNKNOWN_AA) for aa in items)
+
+        def _agg_join_unique(items):
+            return _agg_join(
+                sorted(set(chain(*[str.split(it, ",") for it in items if it])))
+            )
+
+        def _join_aas_resids(row: pd.Series) -> str:
+            return str.join(
+                ",",
+                map(
+                    partial(str.join, ""),
+                    zip(
+                        str.split(row.contact_aas, ","),
+                        str.split(row.contact_resids, ","),
+                    ),
+                ),
+            )
+
+        df_groups = df_filt.groupby(by=["res_id"]).aggregate(
+            {
+                # contacts count and type (unique)
+                "contact": ["count", _agg_join_unique],
+                # distances
+                # note: min and max will ignore nans, and the lambda will count them
+                "distance": [
+                    "min",
+                    "max",
+                ],
+                "contact_sdist": ["min", "max"],
+                # OOC and non-AA contacts
+                "contact_any_ooc": [_agg_join_unique],
+                "contact_non_aa": [_agg_join_unique],
+                # contact AAs and locations
+                "end.label_comp_id": [_agg_join_aas],
+                "end.auth_seq_id": [_agg_join],
+            }
+        )
+
+        df_contacts = df_groups.set_axis(
+            labels=[
+                # count and type
+                "contact_count",
+                "contact_types",
+                # distances
+                "contact_dmin",
+                "contact_dmax",
+                "contact_smin",
+                "contact_smax",
+                # OOC and non-AA contacts
+                "contact_ooc",
+                "contact_non_aa",
+                # contact AAs and locations
+                "contact_aas",
+                "contact_resids",
+            ],
+            axis="columns",
+        )
+
+        # Fix nans
+        df_contacts["contact_count"].fillna(0, inplace=True)
+
+        # Combine the contact AAs and residue ids columns together
+        df_contacts["contact_aas"] = df_contacts.apply(func=_join_aas_resids, axis=1)
+        df_contacts.drop("contact_resids", axis=1, inplace=True)
+
+        df_contacts = (
+            df_contacts.reset_index()
+            .astype(
+                {
+                    "res_id": str,
+                    "contact_count": pd.Int64Dtype(),
+                    "contact_smin": pd.Int64Dtype(),
+                    "contact_smax": pd.Int64Dtype(),
+                }
+            )
+            .set_index("res_id")
+            .sort_values(by=["res_id"])
+        )
+
+        return df_contacts
 
     @classmethod
     def can_execute(
