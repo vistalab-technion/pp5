483d827f407e11c24ecf127aa38e4a1e743293b2
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2024-09-01 05:48:09 +0300

    Refactor: split collect.py into a package

---
+++ src/pp5/collect/__init__.py
@@ -0,0 +1,9 @@
+from pp5.collect.base import (
+    DATASET_DIRNAME,
+    ALL_STRUCTS_FILENAME,
+    COLLECTION_METADATA_FILENAME,
+    ParallelDataCollector,
+)
+from pp5.collect.prec import ProteinRecordCollector
+from pp5.collect.pgroup import ProteinGroupCollector
+from pp5.collect.dataset import ZipDataset, FolderDataset, CollectedDataset
