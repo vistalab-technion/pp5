994d58ce90ac4e5815749ec55014a440826961f4
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2024-09-17 05:37:34 +0300

    unmodelled: refactor to split out extract_unmodelled_context

--- src/pp5/analysis/unmodelled.py
+++ src/pp5/analysis/unmodelled.py
@@ -1,12 +1,16 @@
 from typing import Tuple, Optional, Sequence
 
 import numpy as np
+import pandas as pd
 from numpy import ndarray
 from pandas import DataFrame
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 
 from pp5.stats.cdf import empirical_cdf
+
+BFR_COL = "bfr"
+BBR_COL = "bbr"
 
 
 def extract_unmodelled_segments(
@@ -58,11 +62,94 @@ def extract_unmodelled_segments(
     )
 
 
+def _resolve_column_data(
+    df_prec: DataFrame,
+    col_name: Optional[str] = None,
+    col_data: Optional[ndarray] = None,
+) -> ndarray:
+    """
+    Obtain data from a prec dataframe column, or confirm that a given data array has
+    the appropriate length.
+
+    :param df_prec: The prec dataframe.
+    :param col_name: The name of the column to return data for.
+    :param col_data: Data to use, instead of df_prec[col_name]. If provided,
+    must have the same length as df_prec.
+    :return: The data to use.
+    """
+    if col_data is None:
+        if col_name is None:
+            raise ValueError("Either col_name or col_data must be provided")
+        return np.array(df_prec.loc[:, col_name])
+
+    if [*col_data.shape] != [len(df_prec)]:
+        raise ValueError(f"{col_data.shape=} does not match {len(df_prec)=}")
+
+    return col_data
+
+
+def extract_unmodelled_context(
+    df_prec,
+    context_len: int = 5,
+    col_name: Optional[str] = None,
+    col_data: Optional[np.ndarray] = None,
+) -> DataFrame:
+    """
+    Extracts per-residue data in a context window around unmodelled segments.
+
+    :param df_prec: The prec dataframe.
+    :param context_len: The number of residues to include on each side of the unmodelled
+    segment.
+    :param col_name: The name of the column to extract data from.
+    :param col_data: Data to use, instead of df_prec[col_name]. If provided,
+    must have the same length as df_prec.
+    :return: A dataframe array of n_segments rows, with 2*context_len columns,
+    where each row represents a different unmodelled segment, and columns represent
+    the col_data, with context_len residues before and after the segment.
+    In case there are no residues before/after, the corresponding columns will be NaN.
+    Additional columns will be added for the pdb_id and segment index.
+    """
+    if context_len < 1:
+        raise ValueError("context_len must be at least 1")
+
+    col_data = _resolve_column_data(df_prec, col_name, col_data)
+    unmodelled_segs = extract_unmodelled_segments(df_prec)
+    n_unmodelled = len(unmodelled_segs)
+
+    # Create empty array to store the context around unmodelled segments
+    unmodelled_ctx = np.full((n_unmodelled, 2 * context_len), np.nan)
+
+    for idx_seg, (seg_start_idx, seg_len, _) in enumerate(unmodelled_segs):
+        for j in range(context_len):
+            # Pre-segment context
+            pre_idx = seg_start_idx - context_len + j
+            if pre_idx >= 0:
+                unmodelled_ctx[idx_seg, j] = col_data[pre_idx]
+
+            # Post-segment context
+            post_idx = seg_start_idx + seg_len + j
+            if post_idx < len(col_data):
+                unmodelled_ctx[idx_seg, context_len + j] = col_data[post_idx]
+
+    # Create dataframe
+    offsets = [
+        *[-(i + 1) for i in reversed(range(context_len))],
+        *[(i + 1) for i in range(context_len)],
+    ]
+    offset_cols = [str(o) for o in offsets]
+    df_unmodelled_ctx = DataFrame(unmodelled_ctx, columns=offset_cols)
+    df_unmodelled_ctx["pdb_id"] = df_prec["pdb_id"].iloc[0]
+    df_unmodelled_ctx["seg_idx"] = np.arange(n_unmodelled)
+    df_unmodelled_ctx = df_unmodelled_ctx[["pdb_id", "seg_idx", *offset_cols]]
+    return df_unmodelled_ctx
+
+
 def extract_unmodelled_bfactor_context(
     df_prec, context_len: int = 5, quantiles: bool = True
 ) -> DataFrame:
     """
     Extracts bfactors in a context window around unmodelled segments.
+    See `extract_unmodelled_context` for more details.
 
     :param df_prec: The prec dataframe.
     :param context_len: The number of residues to include on each side of the unmodelled
@@ -75,42 +162,13 @@ def extract_unmodelled_bfactor_context(
     In case there are no residues before/after, the corresponding columns will be NaN.
     Additional columns will be added for the pdb_id and segment index.
     """
-    if context_len < 1:
-        raise ValueError("context_len must be at least 1")
-
-    unmodelled_segs = extract_unmodelled_segments(df_prec)
-    n_unmodelled = len(unmodelled_segs)
-
     bfactors = np.array(df_prec["bfactor"])
     if quantiles:
         bfactors = empirical_cdf(bfactors)
 
-    # Create empty array to store the b-factors
-    bfactors_ctx = np.full((n_unmodelled, 2 * context_len), np.nan)
-
-    for idx_seg, (seg_start_idx, seg_len, _) in enumerate(unmodelled_segs):
-        for j in range(context_len):
-            # Pre-segment context
-            pre_idx = seg_start_idx - context_len + j
-            if pre_idx >= 0:
-                bfactors_ctx[idx_seg, j] = bfactors[pre_idx]
-
-            # Post-segment context
-            post_idx = seg_start_idx + seg_len + j
-            if post_idx < len(bfactors):
-                bfactors_ctx[idx_seg, context_len + j] = bfactors[post_idx]
-
-    # Create dataframe
-    offsets = [
-        *[-(i + 1) for i in reversed(range(context_len))],
-        *[(i + 1) for i in range(context_len)],
-    ]
-    offset_cols = [str(o) for o in offsets]
-    df_bfactors_ctx = DataFrame(bfactors_ctx, columns=offset_cols)
-    df_bfactors_ctx["pdb_id"] = df_prec["pdb_id"].iloc[0]
-    df_bfactors_ctx["seg_idx"] = np.arange(n_unmodelled)
-    df_bfactors_ctx = df_bfactors_ctx[["pdb_id", "seg_idx", *offset_cols]]
-    return df_bfactors_ctx
+    return extract_unmodelled_context(
+        df_prec, context_len=context_len, col_data=bfactors
+    )
 
 
 def plot_with_unmodelled(
@@ -132,10 +190,7 @@ def plot_with_unmodelled(
     :param ax: The axis to plot on. If None, a new figure will be created.
     :param figsize: The size of the figure to create if ax is None.
     """
-    if col_data is None:
-        col_data = np.array(df_prec.loc[:, col_name])
-    elif [*col_data.shape] != [len(df_prec)]:
-        raise ValueError(f"{col_data.shape=} does not match {len(df_prec)=}")
+    col_data = _resolve_column_data(df_prec, col_name, col_data)
 
     if ax is None:
         _, ax = plt.subplots(1, 1, figsize=figsize)
