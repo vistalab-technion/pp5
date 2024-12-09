fd6171492df7e1883b2b17e87e122d83354bd2cb
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2024-12-09 04:48:17 +0200

    unmodelled: refactor into package

---
+++ src/pp5/analysis/unmodelled/__init__.py
@@ -0,0 +1,7 @@
+from pp5.analysis.unmodelled.utils import (
+    plot_with_unmodelled,
+    extract_unmodelled_context,
+    extract_unmodelled_segments,
+    extract_unmodelled_bfactor_context,
+    extract_unmodelled_bfactor_ratio_context,
+)
